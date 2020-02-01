930d06fd3e48b36e013c3e7b629e2a491f8e0744
syed7212 <syedfakhar012@gmail.com>
2020-02-01 15:09:54 -0500

    first commit

---
+++ module-info.java
@@ -0,0 +1,2 @@
+module myJanuaryHomeWork {
+}
---
+++ myJanuaryHomeWork/workingHard.java
@@ -0,0 +1,116 @@
+package myJanuaryHomeWork;
+
+public class workingHard {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+// Arithmetic Operators
+		
+//		int myInt = 12;
+//		int myInt2 = 14;
+//		int myInt3 = 16;
+//		int myInt4 = 20;
+//		
+//		int results = myInt2 + myInt;
+//		int results2 = myInt - myInt2;
+//		int results3 = myInt3 * myInt2;
+//		int results4 = myInt / myInt3;
+//		int results5 = myInt4 % myInt3;
+//		 
+//		int results6 = myInt4++;
+//		int results7 = myInt2--;
+//		
+//		System.out.println("My first result is = " + results);
+//		System.out.println("My second result is = " + results2);
+//		System.out.println("My third result is = " + results3);
+//		System.out.println("My fourth result is = " + results4);
+//		System.out.println("My fifth result is = " + results5);
+//		System.out.println("My sixth result is = " + results6);
+//		System.out.println("My seventh result is = " + results7);
+		
+		
+// Realtional Operator
+		
+//		long myNum = 123;
+//		long myNum2 = 1243;
+//		long myNum3 = 3433;
+//		long myNum4 = 2341;
+//		
+//		boolean answer = myNum == myNum2;
+//		boolean answer2 = myNum2 != myNum;
+//		boolean answer3 = myNum3 > myNum4;
+//		boolean answer4 = myNum4 < myNum;
+//		boolean answer5 = myNum2 >= myNum4;
+//		boolean answer6 = myNum <= myNum2;
+//		 
+//		System.out.println("My first result should be " + answer + ".");
+//		System.out.println("My second result should be " +answer2 + ".");
+//		System.out.println("My third result should be " +answer3 + ".");
+//		System.out.println("My fourth result should be " +answer4 + ".");
+//		System.out.println("My fifth result should be " +answer5 + ".");
+//		System.out.println("My sixth result should be " +answer6 + ".");
+		
+		
+// Concatenation
+		
+//		String firstName = "Syed";
+//		String secName = "Naqvi";
+//		String located = "Fairfax";
+//		
+//		System.out.println("My first name is " + firstName + " and last name is " + secName + " and i live in " + located + ".");
+		
+//Logical Operators
+		
+//		long x = 23;
+//		long y = 24;
+//		long z = 27;
+//		long zz = 27;
+//		
+//		boolean finalAnswer = (x == y)&&(z == x);
+//		boolean finalAnswer1 = (y == zz)||(z == zz);
+//		boolean finalAnswer2 = !(zz == x)&(y == z);		
+//		
+//		System.out.println(finalAnswer);
+//		System.out.println(finalAnswer1);
+//		System.out.println(finalAnswer2);
+		
+          		
+// Assignment Operators		
+//		 
+//		int a = 4;
+//		int b = 8;
+//		int c = 10;
+//		int d = 14;
+//		
+//		
+//		a = a + b;
+//		a+=b;
+//		 
+//		b = b - d;
+//		b-=d;
+//		 
+//		d = d * c;
+//		d*=c;
+//		 
+//		c = c / a;
+//		c/=a;
+//		 
+//		b = b % a;
+//		b%=a;
+//		
+//	
+//		
+//		System.out.println(a);
+//		System.out.println(b);
+//		System.out.println(d);
+//		System.out.println(c);
+//		System.out.println(b);
+		
+		
+		
+		
+	}
+
+}
